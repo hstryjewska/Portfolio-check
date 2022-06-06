51f7a95c4025000cf464d8599f77b7626a664150
hanna stryjewska <hannaolgastryjewska@gmail.com>
2022-06-06 11:01:53 +0200

    .

--- submit.js
+++ submit.js
@@ -1,35 +1,13 @@
-const form = document.getElementById("contact-form");
+const formE = document.getElementById("submit-form");
+formE.onsubmit = async (e) => {
+  e.preventDefault();
 
-async function handleSubmit(event) {
-  event.preventDefault();
-  let status = document.getElementById("status");
-  data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        status.innerHTML = "Thanks for your submission!";
-        console.log(status);
-        form.reset();
-      } else {
-        response.json().then((data) => {
-          if (Object.hasOwn(data, "errors")) {
-            status.innerHTML = data["errors"]
-              .map((error) => error["message"])
-              .join(", ");
-          } else {
-            status.innerHTML = "Oops! There was a problem submitting your form";
-          }
-        });
-      }
-    })
-    .catch((error) => {
-      status.innerHTML = "Oops! There was a problem submitting your form";
-    });
-}
-form.addEventListener("submit", handleSubmit);
+  let response = await fetch("https://formspree.io/f/mayaddnq", {
+    method: "POST",
+    body: new FormData(formE),
+  });
+
+  let result = await response.json();
+
+  alert(result.message);
+};
