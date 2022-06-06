39b99736993483b3ed73e61342f8c4ffaaa2e088
hanna stryjewska <hannaolgastryjewska@gmail.com>
2022-06-06 13:32:29 +0200

    refactor

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
 let error = 0;
-
-// const validateForm = () => {
-//   let nameField = document.forms["contact-form"]["name"].value;
-//   document.getElementById("name_error").innerHTML = "";
-//   if (nameField == null || nameField == "") {
-//     error++;
-//     document.getElementById("name_error").innerHTML = "Name must be filled out";
-//   }
-
-//   let emailField = document.forms["contact-form"]["email"].value;
-//   document.getElementById("email_error").innerHTML = "";
-//   if (emailField == null || emailField == "") {
-//     error++;
-//     document.getElementById("email_error").innerHTML =
-//       "Email must be filled out";
-//   }
-
-//   let ifEmpty = document.forms["contact-form"]["message"].value;
-//   document.getElementById("message_error").innerHTML = "";
-//   if (ifEmpty == null || ifEmpty == "") {
-//     error++;
-//     document.getElementById("message_error").innerHTML =
-//       "Oh no! Please ... write something :)";
-//   }
-
-//   if (error > 0) {
-//     return false;
-//   }
-//   document.getElementById("statusOn").innerHTML = "Sending...";
-// };
-
 const form = document.getElementById("contact-form");
 
 form.addEventListener("submit", handleSubmit);
