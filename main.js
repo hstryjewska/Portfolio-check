fb566968e7684d8d763b3dbfe91bff508b1937e4
hanna stryjewska <hannaolgastryjewska@gmail.com>
2022-06-06 13:24:08 +0200

    submit and validation fixed

--- main.js
+++ main.js
@@ -27,4 +27,21 @@ const validateForm = () => {
   if (error > 0) {
     return false;
   }
+  document.getElementById("statusOn").innerHTML = "Sending...";
+};
+
+const formE = document.getElementById("submit-form");
+const pork = () => {
+  formE.onsubmit = async (e) => {
+    e.preventDefault();
+
+    let response = await fetch("https://formspree.io/f/mayaddnq", {
+      method: "POST",
+      body: new FormData(formE),
+    });
+
+    let result = await response.json();
+
+    alert(result.message);
+  };
 };
--- submit.js
+++ submit.js
@@ -1,13 +1,95 @@
-const formE = document.getElementById("submit-form");
-formE.onsubmit = async (e) => {
-  e.preventDefault();
+let error = 0;
 
-  let response = await fetch("https://formspree.io/f/mayaddnq", {
-    method: "POST",
-    body: new FormData(formE),
-  });
+// const validateForm = () => {
+//   let nameField = document.forms["contact-form"]["name"].value;
+//   document.getElementById("name_error").innerHTML = "";
+//   if (nameField == null || nameField == "") {
+//     error++;
+//     document.getElementById("name_error").innerHTML = "Name must be filled out";
+//   }
 
-  let result = await response.json();
+//   let emailField = document.forms["contact-form"]["email"].value;
+//   document.getElementById("email_error").innerHTML = "";
+//   if (emailField == null || emailField == "") {
+//     error++;
+//     document.getElementById("email_error").innerHTML =
+//       "Email must be filled out";
+//   }
 
-  alert(result.message);
-};
+//   let ifEmpty = document.forms["contact-form"]["message"].value;
+//   document.getElementById("message_error").innerHTML = "";
+//   if (ifEmpty == null || ifEmpty == "") {
+//     error++;
+//     document.getElementById("message_error").innerHTML =
+//       "Oh no! Please ... write something :)";
+//   }
+
+//   if (error > 0) {
+//     return false;
+//   }
+//   document.getElementById("statusOn").innerHTML = "Sending...";
+// };
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", handleSubmit);
+async function handleSubmit(event) {
+  event.preventDefault();
+  const status = document.getElementById("statusOn");
+  const data = new FormData(event.target);
+  let error = 0;
+
+  let nameField = document.forms["contact-form"]["name"].value;
+  document.getElementById("name_error").innerHTML = "";
+  if (nameField == null || nameField == "") {
+    error++;
+    document.getElementById("name_error").innerHTML = "Name must be filled out";
+  }
+
+  let emailField = document.forms["contact-form"]["email"].value;
+  document.getElementById("email_error").innerHTML = "";
+  if (emailField == null || emailField == "") {
+    error++;
+    document.getElementById("email_error").innerHTML =
+      "Email must be filled out";
+  }
+
+  let ifEmpty = document.forms["contact-form"]["message"].value;
+  document.getElementById("message_error").innerHTML = "";
+  if (ifEmpty == null || ifEmpty == "") {
+    error++;
+    document.getElementById("message_error").innerHTML =
+      "Oh no! Please ... write something :)";
+  }
+
+  if (error > 0) {
+    return false;
+  }
+
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok && error <= 0) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
